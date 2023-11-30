5f54f5c3d40b824f4597de60b5b387e5304f8f01
ArtemUah <111749288+ArtemUah@users.noreply.github.com>
2023-11-30 08:22:23 +0000

    Deploying to gh-pages from @ ArtemUah/weatherApi@06a3e48beacf26b76f7faf81105cafe63c793e9c 🚀

---
+++ index.4aa68aad.js
@@ -0,0 +1,2 @@
+console.log("hello"),document.querySelector(".title").textContent=" Goodbuy";
+//# sourceMappingURL=index.4aa68aad.js.map
---
+++ index.6b25c106.js
@@ -0,0 +1,2 @@
+console.log("hello");document.querySelector(".title").textContent=" Goodbuy";
+//# sourceMappingURL=index.6b25c106.js.map
